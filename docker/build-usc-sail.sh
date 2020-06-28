3e0d273948273edf407f06a31f9e13c4fec0b4c7
Arindam Jati <arindamjati@Arindams-MacBook-Pro.local>
2020-06-28 02:42:42 -0700

    minor

--- docker/build-usc-sail.sh
+++ docker/build-usc-sail.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --force-rm --file docker/usc-sail/Dockerfile -t usc-sail/armory:0.1.0 .
+docker build --force-rm --file docker/usc-sail/USCI_LIP_Dockerfile -t usc-sail/armory:0.1.0 .
